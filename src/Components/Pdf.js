0616da1fe3b8cc4a35d2b1665a071c9f4d026bff
Syeda Alishba Kazmi <alishbakazmi99@gmail.com>
2023-11-01 16:44:22 +0500

    new changes pushed

---
+++ src/Components/Pdf.js
@@ -0,0 +1,196 @@
+import React from "react";
+import {
+    Document,
+    Page,
+    Text,
+    View,
+    StyleSheet,
+    PDFViewer,
+    Image,
+} from "@react-pdf/renderer";
+
+import logo from "../Images/logo.png"
+
+const styles = StyleSheet.create({
+    page: {
+        color: "black",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        padding: 30,
+    },
+
+    tableContainer: {
+        display: "flex",
+        flexDirection: "column",
+        width: "80%",
+        // margin: 10,
+        // padding: 7
+    },
+    tableRow: {
+        borderTop: "1px solid #000", // Add a border to the table rows
+        borderRight: "1px solid #000", // Add a border to the table rows
+        borderLeft: "1px solid #000", // Add a border to the table rows
+        // borderBottom: "1px solid #000", // Add a border to the table rows
+        // borderRadius: "1px",
+        flexDirection: "row",
+        margin: "0px",
+    },
+    tableCell: {
+        width: '27%',
+        height: "100%",
+        paddingTop: 7,
+        paddingLeft: 2,
+        // marginBottom: 5,
+        fontSize: 12,
+        // height: "50px",
+        textAlign: "center",
+        borderRight: "1px solid #000", // Add a right border to the table cells
+    },
+    tableCellUpper: {
+        flex: 1,
+        width: "25%",
+        padding: 7,
+        marginBottom: 5,
+        fontSize: 12,
+        borderRight: "1px solid #000", // Add a right border to the table cells
+
+    },
+    headerCell: {
+        backgroundColor: "#ccc",
+        fontWeight: "bold",
+
+    },
+    image: {
+        marginTop: "20px",
+        width: 426,
+        height: 300,
+        borderRadius: "5px"
+    },
+    section: {
+        margin: 10,
+        padding: 10,
+    },
+    viewer: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+    },
+
+    text: {
+        fontSize: 12,
+    },
+    imageContainer: {
+        width: "100%",
+    },
+    header: {
+        fontSize: 16,
+        fontWeight: "bold",
+        marginBottom: 10,
+        textDecoration: "underline",
+    },
+    innerImage: {
+        height: "100px",
+        width: "100px",
+        borderRadius: "2px",
+        margin: "8px",
+    },
+    pageBreak: {
+        flexBasis: "100%",
+        height: 0,
+    },
+    address: {
+        fontSize: 10,
+        fontWeight: "bold",
+
+    },
+});
+
+function Pdf(props) {
+    console.log(props)
+
+
+
+
+    return (
+        <PDFViewer style={styles.viewer}>
+            <Document>
+                <Page size="A4" style={styles.page}>
+                    <View style={{ width: "90%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-around" }} >
+                        <Image style={{ height: "100px", width: "100px" }} src={logo} />
+                        <Text style={styles.header}>Defects Report</Text>
+                        <View style={styles.address}>
+                            <Text>BUDDYFECTS PTE. LTD.</Text>
+                            <Text>UEN: 202318717Z</Text>
+                            <Text>1003 BUKIT MERAH</Text>
+                            <Text>CENTRAL</Text>
+                            <Text>#07-43</Text>
+                            <Text>SINGAPORE (159836)</Text>
+                        </View>
+                    </View>
+                    <View style={styles.tableContainer}>
+                        <View style={[styles.tableRow, styles.headerCell]}>
+                            <Text style={styles.tableCellUpper}>Name</Text>
+                            <Text style={styles.tableCellUpper}>{props.data?.name}</Text>
+                        </View>
+
+                        <View style={styles.tableRow}>
+                            <Text style={styles.tableCellUpper}>Address</Text>
+                            <Text style={styles.tableCellUpper}>{props.data?.address}</Text>
+                        </View>
+                        <View style={styles.tableRow}>
+                            <Text style={styles.tableCellUpper}>Remarks</Text>
+                            <Text style={styles.tableCellUpper}>{props.data?.remarks}</Text>
+                        </View>
+                        <View style={styles.tableRow}>
+                            <Text style={styles.tableCellUpper}>Date</Text>
+                            <Text style={styles.tableCellUpper}> {props.data.date
+                                ? new Date(props.data.date).toISOString().split("T")[0]
+                                : ""}</Text>
+                        </View>
+                    </View>
+                    <Image style={styles.image} src={`http://127.0.0.1:3000/public/image/users/${props.data?.mainImage}`} />
+                </Page>
+
+                {/* Start a new page */}
+                <Page wrap={false} size="A4" style={styles.page}>
+                    <Text style={styles.header}>Inspection Findings Table</Text>
+                    <View style={styles.tableContainer}>
+                        <View style={[styles.tableRow, styles.headerCell]}>
+                            <Text style={styles.tableCell}>S/N</Text>
+                            <Text style={styles.tableCell}>Location</Text>
+                            <Text style={styles.tableCell}>Description</Text>
+                            <Text style={styles.tableCell}>Photo</Text>
+                        </View>
+                        {props.data?.floorDetails?.map((finding, index) => (
+                            <View wrap style={styles.tableRow} key={index}>
+                                <Text style={styles.tableCell}>{index}</Text>
+
+                                <Text style={styles.tableCell}>{finding?.location}</Text>
+
+
+                                <Text style={styles.tableCell}>
+                                    {finding?.description}
+                                </Text>
+                                <View style={{ display: "flex", flexDirection: "column", gap: "10px", justifyContent: "center", alignItems: "center", width: "auto" }}>
+                                    {finding?.img?.map((item, index) => (
+
+                                        <Image
+                                            style={[styles.innerImage]}
+                                            src={`http://127.0.0.1:3000/public/image/users/${item}`}
+                                        />
+                                    ))
+
+                                    }
+                                </View>
+
+                            </View>
+                        ))}
+                    </View>
+                </Page>
+            </Document>
+        </PDFViewer>
+    );
+}
+
+export default Pdf;
--- src/PAGES/Floorplanlayout.js
+++ src/PAGES/Floorplanlayout.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Floorplanlayout.css";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Select from "react-select";
 import { Button } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import logoImg from "../../src/Images/logo33-removebg-preview.png"
+import logoImg from "../../src/Images/logo.png";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
+import Nav from "../Components/Nav/Nav";
 
+import AddIcon from "@mui/icons-material/Add";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
 
 const Floorplanlayout = () => {
   const options = [
@@ -20,189 +27,449 @@ const Floorplanlayout = () => {
   const [count, setCount] = useState([1]);
 
   const [name, setName] = useState("");
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState(new Date());
   const [address, setAddress] = useState("");
   const [remarks, setRemarks] = useState("");
+
+  const [descriptionArr, setDescriptionArr] = useState([""]);
+
   const [location, setLocation] = useState("");
-  const [image, setImage] = useState("");
-  const [print, setPrint] = useState([]);
+  const [locationArr, setLocationArr] = useState([]);
+
+  const [mainImageFile, setMainImageFile] = useState();
+  const [mainImgArr, setMainImgArr] = useState([]);
+  // const [imgArr, setImgArr] = useState([]);
+
+
+
+
 
   const buttonStyle = {
-    color: '#1f4879',
-    borderRadius: '10px', 
-    border: '2px solid lightblue',
+    color: "#1f4879",
+    borderRadius: "10px",
+    border: "2px solid #1f4879",
   };
   const buttonStyleSubmit = {
-    color: 'white',
-    borderRadius: '10px', 
-    border: '2px solid lightblue',
-    textTransform: 'none',
-
-  
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault(); 
-    console.log('Form submitted with data:', { name, date, address, remarks });
+    color: "#1f4879",
+    borderRadius: "10px",
+    border: "2px solid #1f4879",
+    textTransform: "none",
   };
- 
+
+
+  const handleMainImageChange = (e) => {
+    setMainImageFile(e.target.files[0]);
+  };
+
+  const handleDescriptionChange = (e, index) => {
+    const updatedDescriptionArr = [...descriptionArr];
+
+    // Ensure the updated value is assigned to the correct index in the array
+    updatedDescriptionArr[index] = e.target.value;
+
+    // Update the state with the modified array
+    setDescriptionArr(updatedDescriptionArr);
+  };
+
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault()
+    // console.log("images are", mainImgArr)
+    const filteredDescriptionArr = descriptionArr.filter((desc) => desc.trim() !== "");
+
+ locationArr.map((item,index)=>{
+console.log(`location at index ${index} is : `, item)
+})
+
+descriptionArr.map((item,index)=>{
+  console.log(`description at index ${index} is : `, item)
+  })
+
+  mainImgArr.map((item,index)=>{
+    console.log(`img at index ${index} is : `, item)
+    })
+
+
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("address", address);
+    formData.append("remarks", remarks);
+    formData.append("date", date);
+    // formData.append("remarks", location);
+
+    if (mainImageFile) {
+      formData.append("mainImage", mainImageFile);
+    } else {
+      formData.append("mainImage", "");
+    }
+
+    for (const selectedOption of locationArr) {
+      formData.append("location", selectedOption);
+    }
+
+    for (const desc of filteredDescriptionArr) {
+      formData.append("description", desc);
+    }
+
+//     // Loop through each sub-array in mainImgArr
+// mainImgArr.forEach((imgArray, index) => {
+//   console.log(
+//     "imgArray is ", imgArray
+//   )
+//   imgArray.forEach((imgFile, fileIndex) => {
+//     console.log(
+//       "imgFile is ", imgFile
+//     )
+//     formData.append('img', imgFile);
+//     // formData.append(`img[${index}]`, imgFile);
+//   });
+// });
+
+const imgCount = [];
+
+// Loop through each sub-array in mainImgArr
+mainImgArr.forEach((imgArray, index) => {
+  const countArray = [];
+  imgArray.forEach((imgFile, fileIndex) => {
+    countArray.push(imgFile.length);
+    console.log("imgFile is ", imgFile)
+  //   const file = imgFile[fileIndex];
+
+  //   if (file) {
+    for (let i = 0; i < imgFile.length; i++) {
+      const file = imgFile[i];
+      formData.append('img', file);
+    }
+  //   }
+  });
+  imgCount.push(countArray);
+});
+
+// Flatten the imgCount array to get a single array
+const flattenedImgCount = imgCount.reduce((acc, val) => acc.concat(val), []);
+formData.append('imgCount', JSON.stringify(flattenedImgCount));
+
+    // for (const img of imgFiles) {
+    //   if (img) {
+    //     formData.append("img", img);
+    //   } else {
+    //     formData.append("img", ""); // You may want to handle this differently
+    //   }
+    // }
+    // console.log("Form Data Content:");
+
+    // for (const pair of formData.entries()) {
+    //   const [key, value] = pair;
+    //   if (value instanceof File) {
+    //     console.log(key + ": [File]");
+    //   } else {
+    //     console.log(key + ": " + value);
+    //   }
+    // }
+    // "http://54.226.145.28:3000/api/v1/floorplan"
+
+    try {
+      const response = await axios.post('http://127.0.0.1:3000/api/v1/floorplan', formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      console.log("Response from the server:", response.data);
+      // generatePDF();
+      toast.success("Data submitted successfully!!")
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("Something went wrong!")
+    }
+  };
 
   return (
+    <>
+      <Nav />
+      <Box style={{ marginBottom: "20px", padding: "0px" }}>
 
-  <>
-  <Box >
-    <header className='headerWrapper'>
         <Box>
-           <img src={logoImg} />
-
+          <ToastContainer position="top-right" />
+          {/* Same as */}
+          <ToastContainer />
+          <header className="headerWrapper">
+            <Box>
+              <img src={logoImg} />
+            </Box>
+            <Box>
+              <Box className="txtStyle">
+                <p>
+                  BUDDYFECTS PTE. LTD.
+                  <br />
+                  UEN: 202318717Z
+                  <br />
+                  1003 BUKIT MERAH
+                  CENTRAL
+                  #07-43
+                  <br />
+                  SINGAPORE (159836)
+                </p>
+              </Box>
+            </Box>
+          </header>
         </Box>
-        <Box>
-        <Box className="txtStyle">
-        <p>
-        BUDDYFECTS PTE. LTD.<br />
-  UEN: 202318717Z<br />
-  1003 BUKIT MERAH<br />
-  CENTRAL<br />
-  #07-43<br />
-  SINGAPORE (159836)
-        </p>
-  
-</Box>
-        </Box>
-    </header>
-  </Box>
-<form  onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
+          <Box className="Mainwrapper">
+            <Box className="TextfiledFlex">
+              <Box>
+                <label
+                  style={{
+                    margin: "0px",
+                    padding: "0px",
+                    color: "#1f4879",
+                    fontSize: "18px",
+                  }}
+                >
+                  Name
+                </label>
+                <TextField
+                  placeholder="Write Name"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  className="TextfieldStyle"
+                  id="outlined-basic"
+                  variant="outlined"
+                />
+              </Box>
+              <Box>
+                <label
+                  style={{
+                    margin: "0px",
+                    padding: "0px",
+                    color: "#1f4879",
+                    fontSize: "18px",
+                  }}
+                >
+                  Date
+                </label>
+                <div style={{ display: "grid", width: "92%" }}>
+                  <DatePicker
+                    peekNextMonth
+                    showMonthDropdown
+                    showYearDropdown
+                    dropdownMode="select"
+                    className="form-control"
+                    id="date"
+                    name="date"
+                    selected={date}
+                    onChange={(date) =>
+                      setDate(date)
+                    }
+                    dateFormat="yyyy-MM-dd"
+                  />
+                </div>
 
-    <Box className="Mainwrapper">
-        
-      <Box className="TextfiledFlex"  >
-        <Box>
-        <label style={{margin:"0px", padding:"0px",color:"white",fontSize:"18px"}} >Name</label>
-        <TextField
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-            console.log("name is ", name);
-          }}
-          className="TextfieldStyle"
-          id="outlined-basic"
-          variant="outlined"
-        />
+              </Box>
 
-        </Box>
-        <Box>
-        <label style={{margin:"0px", padding:"0px",color:"white",fontSize:"18px"}} >Date</label>
-        <TextField
-          value={date}
-          onChange={(e) => {
-            setDate(e.target.value);
-            console.log("date is", date);
-          }}
-          className="TextfieldStyle"
-          id="outlined-basic"
-          variant="outlined"
-        />
-</Box>
+              <Box>
+                <label
+                  style={{
+                    margin: "0px",
+                    padding: "0px",
+                    color: "#1f4879",
+                    fontSize: "18px",
+                  }}
+                >
+                  Address
+                </label>
+                <TextField
+                  placeholder="Write Address"
 
-<Box>
-        <label style={{margin:"0px", padding:"0px",color:"white",fontSize:"18px"}} >Address</label>
-        <TextField
-          value={address}
-          onChange={(e) => {
-            setAddress(e.target.value);
-            console.log("address is", address);
-          }}
-          className="TextfieldStyle"
-          id="outlined-basic"
-          variant="outlined"
-        />
+                  value={address}
+                  onChange={(e) => {
+                    setAddress(e.target.value);
+                  }}
+                  className="TextfieldStyle"
+                  id="outlined-basic"
+                  variant="outlined"
+                />
+              </Box>
+              <Box>
+                <label
+                  style={{
+                    margin: "0px",
+                    padding: "0px",
+                    color: "#1f4879",
+                    fontSize: "18px",
+                  }}
+                >
+                  Remarks
+                </label>
+                <TextField
+                  placeholder="Write Remarks"
 
-</Box>
-<Box>
-        <label style={{margin:"0px", padding:"0px",color:"white",fontSize:"18px"}} >Remarks</label>
-        <TextField
-          value={remarks}
-          onChange={(e) => {
-            setRemarks(e.target.value);
-            console.log("reamrks is", remarks);
-          }}
-          className="TextfieldStyle"
-          id="outlined-basic"
-          variant="outlined"
-        />
-</Box>
-        <Box>
-            <div  >
+                  value={remarks}
+                  onChange={(e) => {
+                    setRemarks(e.target.value);
+                  }}
+                  className="TextfieldStyle"
+                  id="outlined-basic"
+                  variant="outlined"
+                />
+              </Box>
+              <Box gap={2} style={{ display: "flex", flexDirection: "column" }}>
 
-          <label className="locTxt">Location</label>
-            </div>
-          <Select
-            options={options}
-            value={location}
-            onChange={(selectedOptions) => {
-              setLocation(selectedOptions.value);
-              setPrint((prevPrint) => [...prevPrint, selectedOptions.value]);
 
-              console.log("selected option is", typeof location);
-            }}
-          />
-        </Box>
 
-        {count?.map((item, index) => (
-          <Destription location={location} print={print[index]} />
-        ))}
-        <Box className="btnFlex">
-          <Button
+                <label
+                  style={{
+                    margin: "0px",
+                    padding: "0px",
+                    color: "#1f4879",
+                    fontSize: "18px",
+                  }}
+                >
+                  Main Floor Image
+                </label>
+                <Box style={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
+                  <label htmlFor="main-image-input" className="upload-button">
+                    <input
+                      type="file"
+                      accept="image/*"
+                      id="main-image-input"
+                      onChange={handleMainImageChange}
+                      style={{ display: "none" }}
+                    />
+                    <PhotoCameraIcon fontSize="large" color="primary" />
+                  </label>
+                  {mainImageFile && (
+                    <div>
+                      <img style={{ height: "100px", width: "100px" }} src={URL.createObjectURL(mainImageFile)} alt="Selected" />
+                    </div>
+                  )}
+                </Box>
+              </Box>
            
-            onClick={() => {
-              setCount([...count, count.push(1)]);
-            }}
-            color="primary"
-            style={buttonStyle}
-          >
-            <AddIcon />
-          </Button>
-          <Box >
+              {count?.map((item, index) => (
+                <Destription
+                  key={index}
+                  location={location}
+                  print={locationArr[index]}
+                  // change={handleImgChange}
+                  descChange={(e) => handleDescriptionChange(e, index)} // Pass the index
+                  description={descriptionArr[index]} // Use the description at the specific index
+                  index={index}
+                  // imgFiles={imgFiles}
+                  locationOptions={options}
+                  updateLocationArr={(selectedLocation) => {
+                    const updatedLocationArr = [...locationArr];
+                    updatedLocationArr[index] = selectedLocation.value;
+                    setLocationArr(updatedLocationArr);
+                  }}
 
-          <Button style={buttonStyleSubmit} 
-           type="submit"
-           onClick={handleSubmit}
-           >
-        Submit
-      </Button>
+                  updateImgArr={(destIndex, imgFiles) => {
+                    const updatedMainImgArr = [...mainImgArr];
+                    updatedMainImgArr[destIndex] = imgFiles;
+                    setMainImgArr(updatedMainImgArr);
+                  }}
+
+                />
+              ))}
+              <Box className="btnFlex">
+                <Button
+                  onClick={() => {
+                    setCount([...count, 1]);
+                    setLocationArr([...locationArr, location.value]);
+
+                    // setDescriptionArr([...descriptionArr, description]);
+                  }}
+                  color="primary"
+                  style={buttonStyle}
+                >
+                  <AddIcon />
+                </Button>
+                <Box>
+                  <Button
+                    style={buttonStyleSubmit}
+                    type="submit"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
           </Box>
-      
-        </Box>
+        </form>
       </Box>
-      
-    </Box>
-</form>
-  </>
+
+    </>
   );
 };
 
 const Destription = (props) => {
-  return (
-    <Box className="Flexdiscrip">
-      <Box>
-        <input type="file" />
-      </Box>
-      <p>{props.print}</p>
 
+  const [selectedLocation, setSelectedLocation] = useState(props.locationOptions[0]); // Initialize with the first location option
+  const [imgFiles, setImgFiles] = useState([]); // Store multiple image files for this iteration
+
+  const handleImgChange = (e) => {
+    const selectedFiles = e.target.files;
+    setImgFiles([...imgFiles, selectedFiles]);
+  };
+  
+  useEffect(() => {
+    props.updateImgArr(props.index, imgFiles); // Update the parent component's mainImgArr
+  }, [imgFiles]);
+  return (
+
+    <Box className="Flexdiscrip">
+      <label htmlFor={`image-input-${props.index}`} className="upload-button-small">
+        <input
+          type="file"
+          accept="image/*"
+          multiple
+          id={`image-input-${props.index}`}
+          onChange={handleImgChange}
+          style={{ display: "none" }}
+        />
+        <PhotoCameraIcon fontSize="small" color="primary" />
+      </label>
+      <Box className="location-label">
+        <div>
+          <label className="location-label">
+            Location
+          </label>
+          <Select
+        
+            options={props.locationOptions}
+            value={selectedLocation}
+            onChange={(selectedOption) => {
+              setSelectedLocation(selectedOption);
+              props.updateLocationArr(selectedOption); // Update locationArr in the parent
+            }}
+          />
+        </div>
+      </Box>
       <Box>
         <Box className="Descriptionflex">
-        <Box>
-        <label style={{margin:"0px", padding:"0px",color:"white",fontSize:"18px"}} >Description</label>
-          <TextField
-            className="TextfieldStyle"
-            id="outlined-basic"
-            variant="outlined"
-          />
+          <Box>
+            <label className="description-label">
+              Description
+            </label>
+            <TextField
+              placeholder="Write Description"
+              multiline
+              rows={2}
+              className="TextfieldStyle-small"
+              id={`outlined-basic-${props.index}`}
+              variant="outlined"
+              value={props.description}
+              onChange={(e) => props.descChange(e, props.index)}
+            />
           </Box>
-         
         </Box>
       </Box>
     </Box>
-  );
+  )
 };
 
 export default Floorplanlayout;
